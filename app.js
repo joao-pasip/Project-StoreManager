d02808c6c486902605034b091c85f55dcb9d52ce
Joao Paulo Silva Piauhy <joaopasip@gmail.com>
2022-06-29 14:36:35 -0300

    Iniciando o projeto :rocket:

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -9,5 +10,5 @@ app.get('/', (_request, response) => {
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
